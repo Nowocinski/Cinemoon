dd32ab945966a962eba8d142d6a45d5e0e1f5df6
Nowocinski <vito24222@gmail.com>
2018-10-28 15:18:02 +0100

    Zapoczątkowanie pliku rejestracji

--- rejestracja.php
+++ rejestracja.php
@@ -1,1 +1,53 @@
-SELECT filmy.tytul ,sale.nr_sali, repertuar.czas_rozpoczecia, repertuar.cena_biletu FROM repertuar, sale, filmy WHERE repertuar.id_sali = sale.nr_sali AND repertuar.id_filmu=filmy.tytul
+<?php
+    $title = "Rejestracja";
+    //----------------------------------------------
+    include "side_part/gora.php";
+
+    
+?>
+
+<form class="form" action="rejestracja.php" method="post">
+    <div class="form-group">
+        <label>Imię</label>
+        <input type="text" class="form-control" id="nameField" placeholder="Twoje imię" name="imie" />
+    </div>
+
+    <div class="form-group">
+        <label for="emailField">Nazwisko</label>
+        <input type="text" class="form-control" id="emailField" placeholder="Twój nazwisko" name="nazwisko"/>
+    </div>
+        
+    <div class="form-group">
+        <label for="emailField">E-mail</label>
+        <input type="text" class="form-control" id="emailField" placeholder="Twój adres e-mail" name="email"/>
+    </div>
+        
+    <div class="form-group">
+        <label for="phoneField">Haslo</label>
+        <input type="password" class="form-control" id="phoneField" placeholder="Twoje hasło" name="haslo"/>
+    </div>
+        
+    <div class="form-group">
+        <label for="phoneField">Powtórz haslo</label>
+        <input type="password" class="form-control" id="phoneField" placeholder="Twoje hasło" name="haslo"/>
+    </div>
+        
+    <div class="form-group">
+        <label for="phoneField">Telefon (opcjonalnie)</label>
+        <input type="password" class="form-control" id="phoneField" placeholder="Twój numer telefonu" name="haslo"/>
+    </div>
+
+    <div class="form-group">
+        <label for="phoneField">Typ konta</label>
+        <select name="nazwa">
+            <option>Normalne</option>
+            <option>Studenckie</option>
+        </select>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Wyślij</button> <button type="reset" class="btn btn-default">Wyczyść</button>
+</form>
+
+<?php
+    include "side_part/dol.php";
+?>
