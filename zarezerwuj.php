32875250fa0830a2d153538a1900464e748cb2e6
Nowocinski <vito24222@gmail.com>
2018-11-06 11:36:46 +0100

    Próba dodania wysyłania powiadomienia na e-mail

--- zarezerwuj.php
+++ zarezerwuj.php
@@ -39,14 +39,14 @@
     try
     {
         $polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
-            
+
         if($polaczenie->connect_errno != 0)
             throw new Exception(mysqli_connect_errno());
         else
         {
             $polaczenie->query("SET NAMES utf8");
             $rezultat = $polaczenie->query("SELECT * FROM rezerwacje WHERE id_repertuaru='$id_repertuaru' AND miejsce='$miejsce' AND rzad='$rzad'");
-            
+
             if(!$rezultat)
                 throw new Exception($polaczenie->error);
             else
@@ -56,15 +56,15 @@
                 {
                     $blad_rezerwacji = '<span style="color: red">Przepraszamy, ale ktoś zajoł to miejsce w miendzyczasie.<br>Prosimy wybrać inne.</span>';
                 }
-                
+
                 else
                 {
                     $id_klienta = $_SESSION['id_klienta'];
                     $rezultat2 = $polaczenie->query("SELECT typ FROM klienci WHERE id_klienta='$id_klienta'");
-                    
+
                     if(!$rezultat2)
                         throw new Exception($polaczenie->error);
-                    
+
                     else
                     {
                          $wiersz = $rezultat2->fetch_assoc();
@@ -79,23 +79,57 @@
                         $wiersz = $rezultat3->fetch_assoc();
                         $cena_biletu = $wiersz['cena_biletu'];
                     }
-                    
+
                     if(isset($typ) && isset($cena_biletu))
                     {
                         if($typ == "studencki") $monznik = 0.5;
                         if($typ == "normalny") $mnoznik = 1;
                         $koszt = $cena_biletu * $monznik;
-                        
+
                         if($polaczenie->query("INSERT INTO rezerwacje VALUES (NULL, '$id_klienta', '$id_repertuaru', '$miejsce', '$rzad', '$koszt')"))
                         {
                             $_SESSION['udana_rezerwacja_miejsca'] = true;
+
+                            //Wysyłanie maila na adres klienta
+                            //-------------------------------------------------------------------------
+                            /*require_once('PHPMailer/PHPMailerAutoload.php');
+
+                            $mail = new PHPMailer();
+                            $mail->isSMTP();
+
+                            $mail->SMTPAuth = true;
+                            $mail->SMTPSecure = 'tls';
+                            $mail->Host = 'smtp.gmail.com';
+                            $mail->Port = 587;
+                            $mail->isHTML();
+                            $mail->Username = 'kinocinemoon@gmail.com';
+                            $mail->Password = 'Kino-Cinemoon#2';
+                            $mail->SetFrom('kinocinemoon@gmail.com');
+                            $mail->Subject = 'Cinemoon: Rejestracja przebiegła pomyślnie';
+                            $mail->Body = 'Rezerwacja jest już podpięta do twojego konta.';
+                            $mail->AddAddress($_SESSION['email']);
+
+                            if(!$mail->Send())
+                            {
+                              echo 'Nie działą!<br>';
+                              $_SESSION['wyslanomaila'] = false;
+                              $_SESSION['blad_emaila'] = 'Mailer Error: '.$mail->ErrorInfo;
+                            }
+
+                            else
+                            {
+                              //echo 'Działa!<br>';
+                              $_SESSION['wyslanomaila'] = true;
+                            }*/
+                            //-------------------------------------------------------------------------
+
                             header('Location: dziekujemy.php');
                         }
-                            
+
                         else
                             throw new Exception($polaczenie->error);
                     }
-                    
+
                     else
                         throw new Exception('Nie można zlaleźć typu konta klienta bądź ceny biletu');
                 }
@@ -103,10 +137,10 @@
             $polaczenie->close();
         }
     }
-    
+
     catch(Exception $e)
     {
         echo '<span style="color: red">Błąd serwera. Spróbuj zarejestrować się później</span>';
-        echo '<br>Informacja deweloperska: '.$e;
+        //echo '<br>Informacja deweloperska: '.$e;
     }
 ?>
