e9bbdac94114d285d773ac2bd8a07cce172b0571
Nowocinski <vito24222@gmail.com>
2018-12-08 15:13:43 +0100

    Podlinkowanie edycji kont

--- dodaj-seans.php
+++ dodaj-seans.php
@@ -116,6 +116,7 @@ if (session_status() == PHP_SESSION_NONE)
                   <li class="selected"><a href="dodaj-seans.php"><i class="fa fa-tasks"></i> Nowy seans</a></li>
                   <li><a href="dodaj-film.php"><i class="fa fa-video-camera"></i> Dodaj film</a></li>
                   <li><a href="dodaj-sale.php"><i class="fa fa-university"></i> Dodaj sale</a></li>
+				  <li><a href="edycja-kont.php"><i class="fa fa-id-card" aria-hidden="true"></i> Edycja kont</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right navbar-user">
                      <li class="dropdown user-dropdown">
