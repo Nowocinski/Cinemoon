812f8540eeea5660a403b638d5e0a12ccbe81c40
Nowocinski <vito24222@gmail.com>
2018-12-11 18:04:09 +0100

    Usunięcie niepotrzebnego komentarza

--- adminIT-info.php
+++ adminIT-info.php
@@ -92,16 +92,6 @@ if (session_status() == PHP_SESSION_NONE)
                 </div>
             </div>
             <div class="row">
-                <!--div class="col-md-8">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <h3 class="panel-title"><i class="fa fa-bar-chart-o"></i> Visits Based on a 10 days data</h3>
-                        </div>
-                        <div class="panel-body">
-                            <div id="shieldui-chart1"></div>
-                        </div>
-                    </div>
-                </div-->
 				<div class="col-lg-9">
                     <div class="panel panel-primary">
                         <div class="panel-heading">
