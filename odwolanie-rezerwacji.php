ca1307d0085bec6ddde8c9e1f074abc251de45ed
Nowocinski <vito24222@gmail.com>
2018-12-15 12:04:44 +0100

    Przerobienie skryptu na format PDO

--- odwolanie-rezerwacji.php
+++ odwolanie-rezerwacji.php
@@ -1,53 +1,23 @@
 <?php
-    if (session_status() == PHP_SESSION_NONE)
-        session_start();
-    if(!isset($_SESSION['zalogowany']) || !isset($_POST['przycisk']))
+    if(!isset($_POST['przycisk']))
     {
       header('Location: index.php');
       exit();
     }
 
-    $id_rezerwacji = $_POST['przycisk'];
-    //echo $_POST['przycisk'];
-
     require_once "connect.php";
 
-    //Wyłączenie worningów i włączenie wyświetlania wyjątków
-    mysqli_report(MYSQLI_REPORT_STRICT);
+	try
+	{
+	   $polaczenie = new PDO('mysql:host='.$host.';dbname='.$db_name.';charset=utf8', $db_user, $db_password);
+	}
+	catch(PDOException $e)
+	{
+		echo "Nie można nazwiązać połączenia z bazą danych";
+	}
 
-    try
-    {
-      $polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
-      if($polaczenie->connect_errno != 0)
-          throw new Exception(mysqli_connect_errno());
-      else
-      {
-        $polaczenie->query("SET NAMES utf8");
-        $rezultat = $polaczenie->query("SELECT id_rezerwacji FROM rezerwacje WHERE id_rezerwacji='$id_rezerwacji'");
-
-        if(!$rezultat)
-            throw new Exception($polaczenie->error);
-        else
-        {
-            if($rezultat->num_rows == 0)
-              throw new Exception('Brak rezerwacji w bazie');
-            else
-            {
-              if($polaczenie->query("DELETE FROM rezerwacje WHERE id_rezerwacji='$id_rezerwacji'"))
-              {
-                header('Location: konto.php');
-                exit();
-              }
-              else
-                throw new Exception($polaczenie->error);
-            }
-            $rezultat->free_result();
-        }
-        $polaczenie->close();
-      }
-    }
-    catch (Exception $e)
-    {
-      //echo '<br>Informacja deweloperska: '.$e;
-    }
+	$zapytanie = $polaczenie->prepare("DELETE FROM rezerwacje WHERE {$_POST['przycisk']}");
+	$zapytanie->execute();
+	
+	header('Location: konto.php')
 ?>
