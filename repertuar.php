5fa813277b2e05745bf58d79aeb68f0599dbc442
Nowocinski <vito24222@gmail.com>
2018-12-21 16:46:03 +0100

    Usunięcie błędu powielających się filmów i seansów

--- repertuar.php
+++ repertuar.php
@@ -24,7 +24,7 @@
         {
             // Kodowanie polskich znaków
             $polaczenie->query("SET NAMES utf8");
-            $rezultat = $polaczenie->query("SELECT * FROM filmy INNER JOIN repertuar ON filmy.id_filmu=repertuar.id_filmu GROUP BY filmy.id_filmu ORDER BY filmy.id_filmu DESC");
+            $rezultat = $polaczenie->query("SELECT * FROM filmy INNER JOIN repertuar ON filmy.id_filmu=repertuar.id_filmu WHERE repertuar.czas_rozpoczecia >= CAST(CONCAT(CURDATE(),' ',CURTIME()) as DATETIME) GROUP BY filmy.id_filmu ORDER BY repertuar.czas_rozpoczecia DESC");
             if(!$rezultat)
                 throw new Exception($polaczenie->error);
 
