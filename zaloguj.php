928883dd278be3d9afccca3d3b638f5d79b7479b
Nowocinski <vito24222@gmail.com>
2019-01-11 13:42:52 +0100

    Dodanie opcji logowania się klientów i pracowników z jednego miejsca

--- zaloguj.php
+++ zaloguj.php
@@ -31,6 +31,8 @@
         /* Zapytanie do bazy danych */
         /* mysqli_real_escape_string - zabezpiecza skrypt przed wstrzykiwaniem SQL */
         //$sql = "SELECT * FROM klienci WHERE email='$login' AND haslo='$haslo'";
+		
+		//Logowanie klienta
         if($rezultat = @$polaczenie->query(
             sprintf("SELECT * FROM klienci WHERE email='%s'",
             mysqli_real_escape_string($polaczenie, $login)
@@ -54,6 +56,46 @@
                     unset($_SESSION['blad']);
                     $rezultat->free_result();
                     header('Location: konto.php');
+					exit();
+                }
+            }
+        }
+
+		//Logowanie pracownika
+		if($rezultat = @$polaczenie->query(
+            sprintf("SELECT * FROM pracownicy WHERE email='%s'",
+            mysqli_real_escape_string($polaczenie, $login)
+        )))
+        {
+            $ile_userow = $rezultat->num_rows;
+            if($ile_userow > 0)
+            {
+                /* Tworzenie tablicy asocjacyjnej (skojarzeniowej) */
+                $wiersz = $rezultat->fetch_assoc();
+                if(password_verify($haslo, $wiersz['haslo']))
+                {
+                    $_SESSION['zalogowany'] = true;
+
+					$_SESSION['id_pracownika'] = $wiersz['id_pracownika'];
+					$_SESSION['typ_konta'] = $wiersz['typ_konta'];
+					$_SESSION['imie'] = $wiersz['imie'];
+					$_SESSION['nazwisko'] = $wiersz['nazwisko'];
+					$_SESSION['miejscowosc'] = $wiersz['miejscowosc'];
+					$_SESSION['adres'] = $wiersz['adres'];
+					$_SESSION['email'] = $wiersz['email'];
+					$_SESSION['nr_telefonu'] = $wiersz['nr_telefonu'];
+
+					unset($_SESSION['blad']);
+					$rezultat->free_result();
+					if ( 'administratorIT' == $_SESSION['typ_konta'])
+						header('Location: adminIT-info.php');
+					elseif ( 'pracownik' == $_SESSION['typ_konta'])
+						header('Location: pracownik.php');
+					elseif ( 'menadzerPracownikow' == $_SESSION['typ_konta'])
+						header('Location: menadzer-pracownikow.php');
+					elseif ( 'specjalistaDSObslugi' == $_SESSION['typ_konta'])
+						header('Location: specjalista-ds-obslugi.php');
+					//else ...
                 }
                 
                 else
