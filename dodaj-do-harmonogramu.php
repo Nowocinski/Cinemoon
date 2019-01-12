36610c03eeca2d8349c3167c627ffbf60465cf03
Nowocinski <vito24222@gmail.com>
2019-01-12 17:32:09 +0100

    Poprawienie skryptu

--- dodaj-do-harmonogramu.php
+++ dodaj-do-harmonogramu.php
@@ -17,13 +17,14 @@ catch(PDOException $e)
 	echo "Nie można nazwiązać połączenia z bazą danych";
 }
 
-$zapytanie = $polaczenie->prepare("INSERT INTO harmonogram_prac VALUES ('', :id, :dzien, :czas_od, :czas_do, :opis)");
+$zapytanie = $polaczenie->prepare("INSERT INTO harmonogram_prac VALUES ('', :id, :dzien, :czas_od, :czas_do, :opis, :status)");
 
 $zapytanie->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
 $zapytanie->bindValue(':dzien', $_POST['dzien'], PDO::PARAM_STR);
 $zapytanie->bindValue(':czas_od', $_POST['czas_od'], PDO::PARAM_STR);
 $zapytanie->bindValue(':czas_do', $_POST['czas_do'], PDO::PARAM_STR);
 $zapytanie->bindValue(':opis', $_POST['opis'], PDO::PARAM_STR);
+$zapytanie->bindValue(':status', 0, PDO::PARAM_INT);
 
 $zapytanie->execute();
 
